36015232d10f4e49b606925130eaee4bed8fc350
Tom Wilson <twilson63@gmail.com>
2021-05-27 14:24:20 +0000

    update #27 - handled case where create reaction succeeds but increment cache fails - decided to reject the whole request and remove the successful create

--- core/reactions/index.js
+++ core/reactions/index.js
@@ -4,6 +4,13 @@ const { Async } = require('crocks')
 const cuid = require('cuid')
 const verify = require('../lib/verify')
 
+function rollback(data, id) {
+  return function () {
+    return data.del(reaction.id)
+      .chain(() => Async.Rejected({ ok: false, status: 500, message: 'could not increment cache' }))
+  }
+}
+
 module.exports = (services) => {
   function post(reaction) {
     return Async.of(reaction)
@@ -13,6 +20,7 @@ module.exports = (services) => {
       .chain(reaction =>
         services.data.create(reaction)
           .chain(() => services.cache.inc(`review-${reaction.reviewId}`, reaction.reaction))
+          .bichain(rollback(services.data, reaction.id), Async.Resolved)
           .map(({ ok }) => ({ ok, id: reaction.id }))
       )
       .chain(verify)
