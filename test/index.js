b4649f6eedcb56b93db7525989a05289e735945f
William Ottinger <trip@Williams-MacBook-Pro.local>
2021-05-28 15:34:10 -0400

    tests

--- test/index.js
+++ test/index.js
@@ -2,19 +2,18 @@ require('dotenv').config()
 require('./setup')()
 const fetchMock = require('fetch-mock')
 
-
+const {HYPER_APP_NAME} = process.env
 
 // Mocks
 globalThis.fetch = fetchMock.sandbox()
-  .post('https://dev.hyper63.com/data/twilson63', { status: 201, body: { ok: true }})
-  .put('https://dev.hyper63.com/data/twilson63/movie-1', { status: 200, body: {ok: true }})
-  .post('https://dev.hyper63.com/data/twilson63/_query', { status: 200, body: {ok: true, docs: []}})
-  .post('https://dev.hyper63.com/search/twilson63/_query', { status: 200, body: {ok: true, matches: []}})
-
-
+  .post(`https://dev.hyper63.com/data/${HYPER_APP_NAME}`, { status: 201, body: { ok: true }})
+  .put(`https://dev.hyper63.com/data/${HYPER_APP_NAME}/movie-1`, { status: 200, body: {ok: true }})
+  .get(`https://dev.hyper63.com/data/${HYPER_APP_NAME}/movie-1`, { status: 200, body: {id: "movie-1", title:"caddyshack", year:"1980", actors:[], genre:"comedy", type:"movie"}})
+  .post(`https://dev.hyper63.com/data/${HYPER_APP_NAME}/_query`, { status: 200, body: {ok: true, docs: []}})
+  .post(`https://dev.hyper63.com/search/${HYPER_APP_NAME}/_query`, { status: 200, body: {ok: true, matches: []}})
+  .post(`https://dev.hyper63.com/search/${HYPER_APP_NAME}`, { status: 201, body: {ok: true}})
 
 // Tests
-
 require('./health-check_test.js')
 require('./movies-post-index_test.js')
 require('./movies-put-id_test.js')
